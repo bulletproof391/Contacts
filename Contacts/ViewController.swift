c0241387606aad61e25287674f0b29a05db3f382
Dmitry Vashlaev <vashlaevdev@gmail.com>
2019-10-29 20:59:07 +0200

    #Initial commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Дмитрий Вашлаев on 29/10/2019.
+//  Copyright © 2019 Dmitry Vashlaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
